44b6a0a89beac7322e3bc097181494a0b1216670
Jet <Yotsanon_aim@cmu.ac.th>
2026-01-06 16:00:09 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,35 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum1=0,sum2=0,sum3=1,sum4=0;
+    for(int i=0; i<N; i++){
+        sum1 += A[i];
+    }
+    double mean = sum1 / N;
+    B[0] = mean;
+
+    for(int i=0; i<N; i++){
+        sum2 += pow((A[i] - mean),2);
+    }
+    B[1] = sqrt(sum2 / (N));
+    
+    for(int i=0; i<N; i++){
+        sum3 *= A[i];
+    }
+    B[2] = pow(sum3, (1.0/N));
+
+    for(int i=0; i<N; i++){
+        sum4 += 1.0 / A[i];
+    }
+    B[3] = N / sum4;
+
+    double max = A[0],min = A[0];
+    for(int i=1; i<N; i++){
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    B[4] = max;
+    B[5] = min;
+}
